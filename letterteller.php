a34959c36302eceb8530f2a7d2519d1b9247aa8b
SegerEnd <gj.endendijk@gmail.com>
2025-03-17 13:58:34 +0100

    Padding on container

--- letterteller.php
+++ letterteller.php
@@ -13,7 +13,7 @@
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
             width: 80%;
             max-width: 600px;
-            margin: 3rem auto auto;
+            margin: 3rem auto 3rem auto;
         }
 
         h1 {
